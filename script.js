0a3b888371baaf3c15b8f682e1279764e27f87aa
kmseonhwa <seonhwakei@gmail.com>
2022-03-14 14:58:27 +0900

    server와 client 연결

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-const messageContainer = document.getElementById('message-container');
-const messageForm = document.getElementById('send-container');
-const messageInput = document.getElementById('message-input');
+const socket = io('http://localhost:3000');
 
-const name = prompt('What is your name?');
-appendMessage('You joined');
-
-messageForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const message = messageInput.value;
-    appendMessage(`You: ${message}`);
-    messageInput.value = '';
+socket.on('chat-message', (data) => {
+    console.log(data);
 });
 
-function appendMessage(message) {
-    const messageElement = document.createElement('div');
-    messageElement.innerText = message;
-    messageContainer.append(messageElement);
-}
+// const messageContainer = document.getElementById('message-container');
+// const messageForm = document.getElementById('send-container');
+// const messageInput = document.getElementById('message-input');
+
+// const name = prompt('What is your name?');
+// appendMessage('You joined');
+
+// messageForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     const message = messageInput.value;
+//     appendMessage(`You: ${message}`);
+//     messageInput.value = '';
+// });
+
+// function appendMessage(message) {
+//     const messageElement = document.createElement('div');
+//     messageElement.innerText = message;
+//     messageContainer.append(messageElement);
+// }
