3bc1ca4785a1ee506ccffaa91e783ef0ab328133
kmseonhwa <seonhwakei@gmail.com>
2022-03-15 09:12:38 +0900

    chat rooms - server settings

--- public/script.js
+++ public/script.js
@@ -4,9 +4,19 @@ const messageContainer = document.getElementById('message-container');
 const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
 
-const name = prompt('What is your name?');
-appendMessage('You joined');
-socket.emit('new-user', name);
+if (messageForm != null) {
+    const name = prompt('What is your name?');
+    appendMessage('You joined');
+    socket.emit('new-user', name);
+
+    messageForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const message = messageInput.value;
+        appendMessage(`You: ${message}`);
+        socket.emit('send-chat-message', message);
+        messageInput.value = '';
+    });
+}
 
 socket.on('chat-message', ({ message, name }) => {
     appendMessage(`${name}: ${message}`);
@@ -18,15 +28,6 @@ socket.on('user-connected', (name) => {
 
 socket.on('user-disconnected', (name) => {
     appendMessage(`${name} disconnected`);
-});
-
-messageForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const message = messageInput.value;
-    appendMessage(`You: ${message}`);
-    console.log(socket.id);
-    socket.emit('send-chat-message', message);
-    messageInput.value = '';
 });
 
 function appendMessage(message) {
