87121c59530126ed158f8c65b213e0c9a7b8b186
kmseonhwa <seonhwakei@gmail.com>
2022-03-15 16:50:24 +0900

    chat rooms - socket

--- public/script.js
+++ public/script.js
@@ -1,22 +1,34 @@
 const socket = io('http://localhost:3000');
 
 const messageContainer = document.getElementById('message-container');
+const roomContainer = document.getElementById('room-container');
 const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
 
 if (messageForm != null) {
     const name = prompt('What is your name?');
     appendMessage('You joined');
-    socket.emit('new-user', name);
+    socket.emit('new-user', roomName, name);
 
     messageForm.addEventListener('submit', (e) => {
         e.preventDefault();
         const message = messageInput.value;
         appendMessage(`You: ${message}`);
-        socket.emit('send-chat-message', message);
+        socket.emit('send-chat-message', roomName, message);
         messageInput.value = '';
     });
 }
+
+// 생성된 채팅방을 모든 사용자들의 화면에 동적으로 보여주기
+socket.on('room-created', (room) => {
+    const roomElement = document.createElement('div');
+    roomElement.innerText = room;
+    const roomLink = document.createElement('a');
+    roomLink.href = `/${room}`;
+    roomLink.innerText = 'join';
+    roomContainer.appendChild(roomElement);
+    roomContainer.appendChild(roomLink);
+});
 
 socket.on('chat-message', ({ message, name }) => {
     appendMessage(`${name}: ${message}`);
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-const rooms = { name: {} };
+const rooms = {};
 
 app.get('/', (req, res) => {
     res.render('index', { rooms });
@@ -23,6 +23,7 @@ app.post('/room', (req, res) => {
     rooms[req.body.room] = { users: {} };
     res.redirect(req.body.room);
     // Send message new room was created
+    io.emit('room-created', req.body.room);
 });
 
 app.get('/:room', (req, res) => {
@@ -37,20 +38,40 @@ server.listen(3000);
 const users = {};
 
 io.on('connection', (socket) => {
-    socket.on('new-user', (name) => {
-        users[socket.id] = name;
-        socket.broadcast.emit('user-connected', name);
+    console.log(`connection: ${socket.id}`);
+
+    socket.on('new-user', (room, name) => {
+        socket.join(room);
+        rooms[room].users[socket.id] = name;
+        socket.to(room).broadcast.emit('user-connected', name);
     });
 
-    socket.on('send-chat-message', (message) => {
-        socket.broadcast.emit('chat-message', {
+    socket.on('send-chat-message', (room, message) => {
+        socket.to(room).broadcast.emit('chat-message', {
             message,
-            name: users[socket.id],
+            name: rooms[room].users[socket.id],
         });
     });
 
-    socket.on('disconnet', (name) => {
-        socket.broadcase.emit('user-disconnected', users[socket.id]);
-        delete users[socket.id];
+    socket.on('disconnect', (name) => {
+        // 아래의 socket argument는 뭐지?
+        console.log(`users: ${users}`);
+        console.log(`disconnection: ${socket.id}`);
+        getUserRooms(socket).forEach((room) => {
+            socket
+                .to(room)
+                .broadcast.emit(
+                    'user-disconnected',
+                    rooms[room].users[socket.id]
+                );
+            delete rooms[room].users[socket.id];
+        });
     });
 });
+
+function getUserRooms(socket) {
+    return Object.entries(rooms).reduce((names, [name, room]) => {
+        if (room.users[socket.id] != null) names.push(name);
+        return names;
+    }, []);
+}
